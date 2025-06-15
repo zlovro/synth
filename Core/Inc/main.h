b6c84eb94cd7780eb9d9de636169bbde4801c23c
zlovro <zedniklovro@gmail.com>
2025-06-15 15:33:44 +0200

    fix symlinks 2

---
+++ Core/Inc/main.h
@@ -0,0 +1,170 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+#include <types.h>
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+void delayUs(TIM_TypeDef* pTim, volatile s32 pUs);
+
+typedef pstruct
+{
+    u16           pin;
+    GPIO_TypeDef* bank;
+} gpioPin;
+
+typedef struct
+{
+    u8      id;
+    gpioPin enable;
+} mux;
+
+typedef struct
+{
+    gpioPin pinSig;
+    gpioPin pinsSelect[4];
+} muxMasterCfg;
+
+// typedef struct
+// {
+//     u8 muxId;
+//     u8 muxChan;
+// } muxedPin;
+//
+//     typedef struct
+//     {
+//         muxedPin row, col;
+//     } muxedPinRowCol;
+
+typedef struct
+{
+    u8 row, col;
+} rowColCoord;
+
+#define gpioSet(gpio, state) ((gpio)->bank->BSRR = (gpio)->pin << (state ? 0 : 16))
+#define gpioModeInput(gpio) (gpio)->bank->MODER &= ~((3 << (2 * (gpio)->pin))); (gpio)->bank->PUPDR &= ~(2 << (2 * (gpio)->pin))
+#define gpioModeOutput(gpio) ((gpio)->bank->MODER |= ((1 << (2 * (gpio)->pin))))
+#define gpioGet(gpio) ((gpio)->bank->IDR & (gpio)->pin)
+
+bool muxRead(mux* pMux, u8 pChan);
+void muxWrite(mux* pMux, u8 pChan, bool pVal);
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define usage5 us timer
+#define usage6 48khz dac
+#define LED0_Pin GPIO_PIN_1
+#define LED0_GPIO_Port GPIOA
+#define PITCHBEND_IN_Pin GPIO_PIN_6
+#define PITCHBEND_IN_GPIO_Port GPIOA
+#define MUX_EN_OG_KEY_ROW_Pin GPIO_PIN_8
+#define MUX_EN_OG_KEY_ROW_GPIO_Port GPIOD
+#define MUX_EN_OG_KEY_COL_Pin GPIO_PIN_9
+#define MUX_EN_OG_KEY_COL_GPIO_Port GPIOD
+#define MUX_EN_OG_KBD_ROW_Pin GPIO_PIN_10
+#define MUX_EN_OG_KBD_ROW_GPIO_Port GPIOD
+#define MUX_EN_OG_KBD_COL_Pin GPIO_PIN_11
+#define MUX_EN_OG_KBD_COL_GPIO_Port GPIOD
+#define MUX_EN_KBDV_ROW_Pin GPIO_PIN_12
+#define MUX_EN_KBDV_ROW_GPIO_Port GPIOD
+#define MUX_EN_KBDV_COL_Pin GPIO_PIN_13
+#define MUX_EN_KBDV_COL_GPIO_Port GPIOD
+#define MUX_EN_KEY_ROW_Pin GPIO_PIN_14
+#define MUX_EN_KEY_ROW_GPIO_Port GPIOD
+#define MUX_EN_KEY_COL_Pin GPIO_PIN_15
+#define MUX_EN_KEY_COL_GPIO_Port GPIOD
+#define SPI6_CS_Pin GPIO_PIN_15
+#define SPI6_CS_GPIO_Port GPIOA
+#define MUX_SIG_Pin GPIO_PIN_3
+#define MUX_SIG_GPIO_Port GPIOD
+#define MUX_S0_Pin GPIO_PIN_4
+#define MUX_S0_GPIO_Port GPIOD
+#define MUX_S1_Pin GPIO_PIN_5
+#define MUX_S1_GPIO_Port GPIOD
+#define MUX_S2_Pin GPIO_PIN_6
+#define MUX_S2_GPIO_Port GPIOD
+#define MUX_S3_Pin GPIO_PIN_7
+#define MUX_S3_GPIO_Port GPIOD
+#define BTN_RUN_Pin GPIO_PIN_0
+#define BTN_RUN_GPIO_Port GPIOE
+
+/* USER CODE BEGIN Private defines */
+
+#define TIM_DAC TIM6
+#define TIM_US TIM5
+
+extern const mux gMuxOgKeyRow;
+extern const mux gMuxOgKeyCol;
+extern const mux gMuxOgKbdRow;
+extern const mux gMuxOgKbdCol;
+extern const mux gMuxKbdvRow;
+extern const mux gMuxKbdvCol;
+extern const mux gMuxKeyRow;
+extern const mux gMuxKeyCol;
+
+extern const mux gMuxList[];
+
+extern const muxMasterCfg gMuxMasterCfg;
+
+extern const rowColCoord gOgKeyMap[];
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
