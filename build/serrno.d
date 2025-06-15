526bed9100f4a9af97defcf128032bc24099940b
zlovro <zedniklovro@gmail.com>
2025-06-15 15:47:40 +0200

    fix symlinks 3

--- build/serrno.d
+++
@@ -1,4 +0,0 @@
-build/serrno.o: Core/Inc/sinf/serrno.c Core/Inc/sinf/serrno.h \
- Core/Inc/sinf/types.h
-Core/Inc/sinf/serrno.h:
-Core/Inc/sinf/types.h:
--- build/sfs.d
+++
@@ -1,5 +0,0 @@
-build/sfs.o: Core/Inc/sfs/sfs.c Core/Inc/sfs/sfs.h Core/Inc/sinf/serrno.h \
- Core/Inc/sinf/types.h
-Core/Inc/sfs/sfs.h:
-Core/Inc/sinf/serrno.h:
-Core/Inc/sinf/types.h:
